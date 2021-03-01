c8e561ec4812f7861b8e6d112dfe27655e2217ad
Prakash Tripathi <prakashtripathi2019@gmail.com>
2021-03-01 13:44:18 +0530

    Third commit

---
+++ src/testcom/FirstSeleniumTest.java
@@ -0,0 +1,10 @@
+package testcom;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+public class FirstSeleniumTest {
+	
+public static void main(String[]args) {
+	WebDriver driver =new FirefoxDriver();
+	driver.get("https://www.oracle.com/index.html");
+}
+}
